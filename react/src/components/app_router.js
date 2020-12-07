9c9f2eecef848ae3a34b0f011894502e9c5ea9fa
Samy Boutouis <sb590@duke.edu>
2020-12-07 16:02:52 -0600

    react controller

---
+++ react/src/components/app_router.js
@@ -0,0 +1,39 @@
+import React ,{Component} from "react";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './home';
+
+import OAuth from './oauth/login';
+
+
+class AppRouter extends Component {
+
+  render(){
+    return (
+      <div>
+        <Router>
+          <div>
+            {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/oauth/consume">
+                <OAuth />
+              </Route>
+              
+              {/* note that due to control flow the root path is at the bottom by design */}
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default AppRouter;
