325e24952ff83e90596599d6c50f43c81ddfd1e7
Anna Mollard <acmollard@gmail.com>
2020-12-09 13:16:45 -0500

    matching shifts (i think)

--- express/scheduler.js
+++ express/scheduler.js
@@ -36,27 +36,51 @@ exports.schedule = async function() {
       }
   }
 
-  exports.schedule3 = async function(group) {
+//   exports.schedule3 = async function(group) {
+//     try {
+//         return await usersCollection.aggregate([
+//             { $match: {group: group} },
+//             { $sort: {rank: 1} },
+//             { $unwind: { path: "$availability.times", preserveNullAndEmptyArrays: true } },
+//             { $lookup: {
+//                 "from": "shifts",
+//                 "let": { "group": "$group", "start_time": '$start_time', "end_time": '$end_time' },
+//                 "pipeline": [
+//                     { $match: { "group": "$$group", "status": "open", "availability.times.start_time": { $lte: "$$start_time" }, "availability.times.end_time": { $gte: "$$end_time" } } }
+//                 ],
+//                 "as": "matching_shifts"
+//             }}
+//         ]).toArray();
+//       } catch (err) {
+//         console.log(err);
+//       }
+//   }
+
+exports.schedule3 = async function(group) {
     try {
-        let users = await usersCollection.aggregate([
-            { $match: {group: group}},
-            { $sort: {rank: 1}}
+        return await usersCollection.aggregate([
+            { $match: {group: group} },
+            { $sort: {rank: 1} },
+            { $unwind: { path: "$availability.times", preserveNullAndEmptyArrays: true } },
+            { $lookup: {
+                "from": "shifts",
+                "let": { "start_time": '$availability.times.start_time', "end_time": '$availability.times.end_time' },
+                "pipeline": [
+                    { $match: { $expr: { $and: [ 
+                        { $eq: [ "$group", group ] },
+                        { $eq: [ "$status", "open" ] },
+                        { $lte: [ "$$start_time", "$end_time" ] },
+                        { $gte: [ "$$end_time", "$start_time" ] } ] } } }
+                ],
+                "as": "matching_shifts"
+            }}
         ]).toArray();
-        let shifts = new Array();
-        for (const user of users) {
-          let u = user;
-          let s = await shiftsCollection.find({"group": group, "status":"open"} ).toArray();
-          // let s = await shiftsCollection.find({"group": group, "start_time": { $gte: user.availability.times.start_time }, "end_time": { $lte: user.availability.times.end_time } } ).toArray();
-          u.shifts = s;
-          shifts.push(u);
-        }
-        return shifts;
       } catch (err) {
         console.log(err);
       }
   }
 
-//   exports.schedule2 = async function(group) {
+//   exports.schedule4= async function(group) {
 //     try {
 //         return await shiftsCollection.aggregate([
 //         { "$match": { "group": group, "status": "open"} },
