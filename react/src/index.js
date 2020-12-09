4d04addf39f793fad80f9c1ce07d02fca6fe3995
Samy Boutouis <sb590@duke.edu>
2020-12-09 08:58:42 -0600

    bootstrap columns

--- react/src/index.js
+++ react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // TODO consider doing something like this to handle global host url
 // in addition to adding auth to all you requests by default
@@ -10,9 +11,15 @@ import * as serviceWorker from './serviceWorker';
 // axios.defaults.headers.common['Authorization'] = bearerToken();
 
 ReactDOM.render(
-  <React.StrictMode>
+  (<React.StrictMode>
     <App />
-  </React.StrictMode>,
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.3/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+      crossorigin="anonymous"
+    />
+  </React.StrictMode>),
   document.getElementById('root')
 );
 
