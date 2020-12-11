bcb0b33576d385b6193d0125b2c7c7d01e7d1c72
taiyangie <sunnyxli2019@gmail.com>
2020-12-11 16:10:20 -0600

    sighs. the buttons dONT WORK

--- react/src/components/schedule/dayWeekMonth.js
+++ react/src/components/schedule/dayWeekMonth.js
@@ -1,36 +1,25 @@
-import React, {Component} from 'react';
-
-class DayWeekMonth extends Component {
-  constructor(props){
-    super();
-    this.state = {currentPage: 'Week'}
-  }
-
-  changeCurrentPage = (newPage) => {
-    this.props.setNavState(newPage)
-  }
+import React, { useState } from "react";
 
 
-  tabBar = () => {
-    return(
-        <div className="dwm">
-          {/* <ToggleButtonGroup className="view" type="radio" name="options" defaultValue={2}>
-            <ToggleButton value={1} className="day-week-month" onClick={this.changeCurrentPage.bind(this, "Day")}>Day</ToggleButton>
-            <ToggleButton value={2} className="day-week-month" onClick={this.changeCurrentPage.bind(this, "Week")}>Week</ToggleButton>
-            <ToggleButton value={3} className="day-week-month" onClick={this.changeCurrentPage.bind(this, "Month")}>Month</ToggleButton>
-          </ToggleButtonGroup> */}
-        </div>
+const DayWeekMonth = () => {
+const[isActive, setActive] = useState("false");
 
-    )
-  }
+const handleToggle1 = () =>{
+  setActive(!isActive);
+};
+const handleToggle2 = () =>{
+  setActive(!isActive);
+};
+const handleToggle3 = () =>{
+  setActive(!isActive);
+};
+return (
+  <div>
+    <button  className={isActive ? "day-week-month active" : "day-week-month"} onClick={handleToggle1}> Day </button>
+    <button  className={isActive ? "day-week-month active" : "day-week-month"} onClick={handleToggle2}> Week</button>
+    <button  className={isActive ? "day-week-month active" : "day-week-month"} onClick={handleToggle3}> Month </button>
+  </div>
+);
+};
 
-  render(){
-    return(
-      <div>
-        {this.tabBar()}
-      </div>
-    )
-  }
-}
-
-export default DayWeekMonth
+export default DayWeekMonth;
