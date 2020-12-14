d3957e79f88693c0ea6932928e6696f6a78c72dd
taiyangie <sunnyxli2019@gmail.com>
2020-12-14 11:14:33 -0600

    the buttons function...but at what cost

--- react/src/components/schedule/dayWeekMonth.js
+++ react/src/components/schedule/dayWeekMonth.js
@@ -1,25 +1,67 @@
-import React, { useState } from "react";
+import React, {Component} from 'react';
+
+class DayWeekMonth extends Component {
+  constructor(props){
+    super();
+    this.state = {currentPage: 'Week'}
+  }
+
+  changeCurrentPage = (newPage) => {
+    this.props.setNavState(newPage)
+  }
+
+  tabBar = () => {
+    return(
+        <div className="dwm">
+          <div className="view">
+            <button className="day-week-month active" onClick={this.changeCurrentPage.bind(this, "Day")}>Day</button>
+            <button className="day-week-month active" onClick={this.changeCurrentPage.bind(this, "Week")}>Week</button>
+            <button className="day-week-month active" onClick={this.changeCurrentPage.bind(this, "Month")}>Month</button>
+          </div>
+        </div>
+
+    )
+  }
+
+  render(){
+    return(
+      <div>
+        {this.tabBar()}
+      </div>
+    )
+  }
+}
+
+export default DayWeekMonth
+
+
+
+/*import React, { useState } from "react";
 
 
 const DayWeekMonth = () => {
 const[isActive, setActive] = useState("false");
+const[isActive2, setActive2] = useState("false");
+const[isActive3, setActive3] = useState("false");
 
-const handleToggle1 = () =>{
+const handleToggle = () =>{
   setActive(!isActive);
 };
+
 const handleToggle2 = () =>{
-  setActive(!isActive);
+  setActive2(!isActive2);
 };
 const handleToggle3 = () =>{
-  setActive(!isActive);
+  setActive3(!isActive3);
 };
+
 return (
-  <div>
-    <button  className={isActive ? "day-week-month active" : "day-week-month"} onClick={handleToggle1}> Day </button>
-    <button  className={isActive ? "day-week-month active" : "day-week-month"} onClick={handleToggle2}> Week</button>
-    <button  className={isActive ? "day-week-month active" : "day-week-month"} onClick={handleToggle3}> Month </button>
+  <div className="dwm">
+    <button  className={isActive ? "day-week-month" : "day-week-month active"} onClick={handleToggle}> Day </button>
+    <button  className={isActive2 ? "day-week-month" : "day-week-month active"} onClick={handleToggle2}> Week</button>
+    <button  className={isActive3 ? "day-week-month" : "day-week-month active"} onClick={handleToggle3}> Month </button>
   </div>
 );
 };
 
-export default DayWeekMonth;
+export default DayWeekMonth;*/
