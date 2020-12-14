0c5f2b76c7321e373944f65f8b3940efaf1dee68
Samy Boutouis <sb590@duke.edu>
2020-12-14 11:25:45 -0600

    working button

--- react/src/components/user/employee_home.js
+++ react/src/components/user/employee_home.js
@@ -11,17 +11,17 @@ class EmployeeHome extends Component {
 
   render() {
     return (
-      <div class="tile is-ancestor">
-        <div class="tile is-7 is-vertical is-parent">
-          <div class="tile is-child">
+      <div className="tile is-ancestor">
+        <div className="tile is-7 is-vertical is-parent">
+          <div className="tile is-child">
             <CurrentShift />
           </div>
-          <div class="tile is-child">
+          <div className="tile is-child">
             <UpcomingShifts />
           </div>
         </div>
-        <div class="tile is-parent">
-          <div class="tile is-child">
+        <div className="tile is-parent">
+          <div className="tile is-child">
             <ShiftPool />
           </div>
         </div>
