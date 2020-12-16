bf84cdd90efccaa6afc374e6dd95aaaad9aa0fcf
Samy Boutouis <sb590@duke.edu>
2020-12-16 09:24:45 -0600

    add cors request

--- express/index.js
+++ express/index.js
@@ -21,17 +21,17 @@ db.connect(() => {
     })
   ); //initialize session
 
-  //for httponly cookies
-  const cookieParser = require('cookie-parser');
-  app.use(cookieParser());
-
-  app.use(cors()) //allow cors requests
+  app.use(cors({credentials: true, origin: 'http://localhost:3000'})) //allow cors requests
   app.use(express.json()); //this line allows us to read JSON bodies in the request
 
   const bodyParser = require('body-parser');
   app.use(bodyParser.json()); // support json encoded bodies
   app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
+  
+  //for httponly cookies
+  const cookieParser = require('cookie-parser');
+  app.use(cookieParser());
+  
   // for parsing multipart/form-data
   app.use(upload.array()); 
   app.use(express.static('public'));
