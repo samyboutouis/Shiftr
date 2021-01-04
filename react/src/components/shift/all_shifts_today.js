90ca9563e5f141874a84f5d84888ccd895d49deb
Samy Boutouis <sb590@duke.edu>
2021-01-04 15:47:40 -0600

    basic clock in/out functionality

--- react/src/components/shift/all_shifts_today.js
+++ react/src/components/shift/all_shifts_today.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import format from 'date-fns/format'
 import Clock from '../../clock.png';
+import getUnixTime from 'date-fns/getUnixTime'
 
 class AllShiftsToday extends Component {
   constructor(props){
     super(props);
+    this.state = {clockedIn: false};
   }
 
   drawShifts = () => {
@@ -19,7 +22,6 @@ class AllShiftsToday extends Component {
 
   mapShifts = () => {
     let shifts = this.props.shifts;
-    shifts.sort((a, b) => a.start_time - b.start_time);
     let timeFormat = "hh:00";
     let pm = "a";
     return shifts.map((shift,index) => 
@@ -37,6 +39,24 @@ class AllShiftsToday extends Component {
     );
   }
 
+  handleClick = () => {
+    let self = this;
+    let time = getUnixTime(Date.now());
+    if(this.state.clockedIn){
+      axios.put("http://localhost:8080/shifts/update/" + this.props.shifts[0]._id, {clocked_out: time}).then((response) => {
+        self.setState({clockedIn: false});
+      }).catch(function (err){  
+        console.log(err)
+      });
+    } else {
+      axios.put("http://localhost:8080/shifts/update/" + this.props.shifts[0]._id, {clocked_in: time}).then((response) => {
+        self.setState({clockedIn: true});
+      }).catch(function (err){  
+        console.log(err)
+      });
+    }
+  }
+
   render() {
     let zeroShifts = "Enjoy the day off!"
     if(this.props.numOfShifts === 0){
@@ -50,10 +70,16 @@ class AllShiftsToday extends Component {
       let shift = [];
       shift.push(<div key="shifts">{this.drawShifts()}</div>);
       if(this.props.affiliation === 'student'){
+        let message = "";
+        if(this.state.clockedIn){
+          message = "Clock Out";
+        } else {
+          message = "Clock In";
+        }
         shift.push(
-          <button className="clock-in" key="button"> 
+          <button className="clock-in" key="button" onClick={this.handleClick}> 
             <img className="clock" src={Clock} alt="Clock"/>
-            <span className="clock-text">Clock In</span>
+            <span className="clock-text">{message}</span>
           </button>
         );
       }
@@ -64,7 +90,6 @@ class AllShiftsToday extends Component {
       );
     }
   }
-
 }
 
 export default AllShiftsToday;
